afc0adbb4e799d683d5a7da38a52b98b79f9a9a6
garthtarr <garth.tarr@gmail.com>
2014-11-10 19:40:34 +1100

    v0.4.7 with new data sets

--- R/mplot-package.R
+++ R/mplot-package.R
@@ -31,6 +31,67 @@ NULL
 NULL
 
 
+#' Blood and other measurements in diabetics
+#' 
+#' The diabetes data frame has 442 rows and 11 columns. 
+#' These are the data used in Efron et al. (2003).
+#' 
+#' @name diabetes
+#' @format A data frame with 442 observations on 11 variables.
+#' @details Data sourced from http://web.stanford.edu/~hastie/Papers/LARS
+#' @docType data
+#' @keywords datasets
+#' @usage data(diabetes)
+#' @references Efron, B., Hastie, T., Johnstone, I., Tibshirani, R., (2004).
+#'   Least angle regression. The Annals of Statistics 32(2) 407-499.
+#'   DOI: 10.1214/009053604000000067
+#' @examples
+#' data(diabetes)
+#' full.mod = lm(y~.,data=diabetes)
+NULL
+
+
+
+
+#' Blood and other measurements in diabetics
+#' 
+#' An artificial data set which causes stepwise regression
+#' procedures to select a non-parsimonious model.
+#' The true model is a simple linear regression of
+#' y against x8.
+#' 
+#' @name artificialeg
+#' @format A data frame with 50 observations on 10 variables.
+#' @details Inspired by the pathoeg data set in the MPV pacakge.
+#' @docType data
+#' @keywords datasets
+#' @usage data(artificialeg)
+#' @examples
+#' data(artificialeg)
+#' full.mod = lm(y~.,data=df)
+#' step(full.mod)
+#' \dontrun{
+#' # generating model
+#' n=50
+#' set.seed(8) # a seed of 2 also works
+#' x1 = rnorm(n,0.22,2)
+#' x7 = 0.5*x1 + rnorm(n,0,sd=2)
+#' x6 = -0.75*x1 + rnorm(n,0,3)
+#' x3 = -0.5-0.5*x6 + rnorm(n,0,2)
+#' x9 = rnorm(n,0.6,3.5)
+#' x4 = 0.5*x9 + rnorm(n,0,sd=3)
+#' x2 = -0.5 + 0.5*x9 + rnorm(n,0,sd=2)
+#' x5 = -0.5*x2+0.5*x3+0.5*x6-0.5*x9+rnorm(n,0,1.5)
+#' x8 = x1 + x2 -2*x3 - 0.3*x4 + x5 - 1.6*x6 - 1*x7 + x9 +rnorm(n,0,0.5)
+#' y = 0.6*x8 + rnorm(n,0,2)
+#' df = round(data.frame(x1,x2,x3,x4,x5,x6,x7,x8,x9,y),1)
+#' }
+NULL
+
+
+
+
+
 #' Extract model elements
 #' 
 #' This function extracts things like the formula,
